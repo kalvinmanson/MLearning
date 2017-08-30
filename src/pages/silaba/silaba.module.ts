1ae7430248beab54b583215b5d6c697d7387eec3
Kalvin Manson <kalvinmanson@gmail.com>
2017-08-30 17:36:56 -0500

    Silaba component

---
+++ src/pages/silaba/silaba.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { SilabaPage } from './silaba';
+
+@NgModule({
+  declarations: [
+    SilabaPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(SilabaPage),
+  ],
+})
+export class SilabaPageModule {}
